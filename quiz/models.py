381d6f66499d8bc2b3c6ac4d26f7a0e3155a98f3
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-04-30 21:39:32 +0530

    question answer check view

--- quiz/models.py
+++ quiz/models.py
@@ -6,22 +6,22 @@ import datetime
 class Question(models.Model):
     question=models.CharField(max_length=550)
     day=models.IntegerField()
-    level_no=models.IntegerField()
+    question_no=models.IntegerField()
     answer=models.CharField(max_length=100)
     audiofield=models.FileField(upload_to='media',blank=True)
     image=models.ImageField(upload_to='media/images',blank=True)
+    hint=models.CharField(max_length=555,default='na')
     def __str__(self):
         return "{}-Day{}".format(self.question,self.day)
 
     class Meta:
-        ordering=['day']
+        ordering=['day','question_no']
 
-class Answer(models.Model):
-    question=models.ForeignKey(to=Question,on_delete=models.CASCADE,related_name='qa')
-    answer=models.CharField(max_length=100)
-    user=models.ForeignKey(to=User,on_delete=models.CASCADE)
-    timestamp=models.DateTimeField(auto_now_add=True)
+    def check_ans(self,answer,question):
+        if question.answer==answer:
+            return True
 
+           
 class UserScore(models.Model):
     user=models.ForeignKey(to=User,on_delete=models.CASCADE)
     name=models.CharField(max_length=55,null=True)
@@ -39,6 +39,10 @@ class UserScore(models.Model):
             player.rank=rank
             rank +=1
         return players
+
+    def new_score(self,player):
+        player.score+=10
+        player.save()
 
 
 class config(models.Model):
