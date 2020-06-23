2834c7bfa281b7bd39f5886c5a094b627185d394
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-06-23 13:17:07 +0530

    settings change

--- screencast/settings.py
+++ screencast/settings.py
@@ -213,7 +213,7 @@ SOCIAL_AUTH_PIPELINE = (
 'social_core.pipeline.user.user_details', )
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
