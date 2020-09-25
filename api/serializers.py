0716850049a61cd446d1fc12b7c2695b07eccc30
Kaustuv942 <kaustuv942@gmail.com>
2020-09-26 00:15:13 +0530

    ok

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,7 @@ class LeaderboardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=UserScore
-        fields=['user','name','email','score','rank']
+        fields=['user','name','imgurl','email','score','rank']
 
 class QuestionSerializer(serializers.ModelSerializer):
 
