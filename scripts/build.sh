ff565f54aa7e4fda99d16014c72df5e8b9cfa5f6
Kaustuv942 <kaustuv942@gmail.com>
2020-09-27 16:06:04 +0530

    script ok

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 cd .. 
 
 [ -d venv ] && echo "File exist" || (echo "creating virtualenv" && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt)
-
+[ -f .env ] && echo "File exist" || (touch .env) && cp .env.example .env
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
