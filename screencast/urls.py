69c3421751e7ae435a334b404e29275f11cf4b47
Kaustuv942 <kaustuv942@gmail.com>
2020-09-27 19:48:13 +0530

    debug false media error

--- screencast/urls.py
+++ screencast/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from settings import DEBUG
 
-if DEBUG == True:
+
+if settings.DEBUG == True:
     urlpatterns = [
         path('admin/', admin.site.urls),
         path('api/',include('api.urls')),
