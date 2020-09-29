2e2fcf57a3fc1d38f4768ec77e98cdf050604c68
Kaustuv942 <kaustuv942@gmail.com>
2020-09-29 17:34:34 +0530

    Homepage ok3

--- screencast/settings.py
+++ screencast/settings.py
@@ -142,12 +142,15 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-if DEBUG:
-    STATICFILES_DIRS = [
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static'),
+#     ]
+# else:
+#     STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATIC_ROOT=os.path.join(BASE_DIR,'static')
+]
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
