d6d76426d2576bc6d3f1ae4cb5db5c4fd120482a
Kaustuv942 <kaustuv942@gmail.com>
2020-09-29 17:43:41 +0530

    test debug false

--- screencast/settings.py
+++ screencast/settings.py
@@ -148,9 +148,7 @@ USE_TZ = False
 #     ]
 # else:
 #     STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
