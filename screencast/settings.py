eb8c17ff48bc17a4dea8ebbc35ccd95d4332dab1
Kaustuv942 <kaustuv942@gmail.com>
2020-10-02 21:34:25 +0530

    ok

--- screencast/settings.py
+++ screencast/settings.py
@@ -145,7 +145,7 @@ if DEBUG:
     
 else:
     STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_URL ='/static/'
+STATIC_URL ='static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
